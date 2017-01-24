ad56db8243c580a603e952dffd65168b3f98475a
Hongyi Guo <guohongyi1991@gmail.com>
2017-01-24 14:25:50 -0500

    v1.2

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -1,12 +1,17 @@
 //Controllers
-weatherApp.controller('homeController', ['$scope', 'cityService',
-function($scope, cityService){
+weatherApp.controller('homeController', ['$scope', '$location','cityService',
+function($scope, $location, cityService){
 
     $scope.city = cityService.city;
     
     $scope.$watch('city', function(){
         cityService.city = $scope.city;
     });
+    
+    $scope.submit = function(){
+        $location.path("/forecast");
+    };
+    
 }]);
 
 weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams','cityService', 
