0c6be56fd2876464ec88474c8dd1a2e1ce7ad202
Hongyi Guo <guohongyi1991@gmail.com>
2017-01-24 16:53:42 -0500

    v1.4

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -14,16 +14,14 @@ function($scope, $location, cityService){
     
 }]);
 
-weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams','cityService', 
-function($scope, $resource, $routeParams, cityService){
+weatherApp.controller('forecastController', ['$scope', '$routeParams', 'cityService', 'weatherService',
+function($scope, $routeParams, cityService, weatherService){
     
     $scope.city = cityService.city;
     
     $scope.days = $routeParams.days || '2';
     
-    $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily?APPID=6842a7042b38290884e0ae5a8a2baedc", {get: {method: "JSONP"}});
-    
-    $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days});
+    $scope.weatherResult = weatherService.GetWeather($scope.city, $scope.days);
     
     $scope.convertToFahrenheit = function(degK){
         
--- scripts/services.js
+++ scripts/services.js
@@ -2,3 +2,14 @@
 weatherApp.service('cityService', function(){
     this.city = "Dallas, TX";
 });
+
+weatherApp.service('weatherService', ['$resource',function($resource){
+   
+    this.GetWeather = function(city, days){
+        
+        var weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily?APPID=6842a7042b38290884e0ae5a8a2baedc", {get: {method: "JSONP"}});
+
+        return weatherAPI.get({ q: city, cnt: days});        
+    };
+
+}]);
