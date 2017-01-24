14ddbb06d6041669d2c2c82c288ad21112106ff5
Hongyi Guo <guohongyi1991@gmail.com>
2017-01-24 11:30:11 -0500

    Multiple Days

--- app.js
+++ app.js
@@ -15,6 +15,11 @@ weatherApp.config(function($routeProvider){
        controller: 'forecastController'
    })   
    
+   .when('/forecast/:days', {
+       templateUrl: 'pages/forecast.htm',
+       controller: 'forecastController'
+   })
+   
 });
 
 //Services
@@ -34,15 +39,25 @@ function($scope, cityService){
     });
 }]);
 
-weatherApp.controller('forecastController', ['$scope', '$resource', 'cityService', 
-function($scope, $resource, cityService){
+weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams','cityService', 
+function($scope, $resource, $routeParams, cityService){
     
     $scope.city = cityService.city;
     
+    $scope.days = $routeParams.days || 2;
+    
     $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily?APPID=6842a7042b38290884e0ae5a8a2baedc", {get: {method: "JSONP"}});
     
-    $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: 2});
+    $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days});
     
-    console.log($scope.weatherResult);
+    $scope.convertToFahrenheit = function(degK){
+        
+        return Math.round(1.8 * (degK - 273) + 32);
+    }
+    
+    $scope.convertToDate = function(dt){
+        
+        return new Date(dt * 1000);
+    };
     
 }]);
